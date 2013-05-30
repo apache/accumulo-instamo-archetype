75dbbd63f055e99e69d0e46b15d8777dc98ac351
Keith Turner <kturner@apache.org>
2013-05-30 22:11:41 +0000

    ACCUMULO-1166 improved instamo example unit test
    git-svn-id: https://svn.apache.org/repos/asf/accumulo/contrib/instamo-archetype/branches/1.4@1488037 13f79535-47bb-0310-9956-ffa450edef68

--- README.md
+++ README.md
@@ -13,7 +13,9 @@ Usage
 Checkout this project and run a `mvn install`. This will install the archetype
 to your local repository.
 
-Then, change to a new directory and run `mvn archetype:generate
--DarchetypeGroupId=org.apache.accumulo -DarchetypeArtifactId=instamo-archetype`
+Then, change to a new directory and run 
+
+`mvn archetype:generate -DarchetypeGroupId=org.apache.accumulo -DarchetypeArtifactId=instamo-archetype -DinteractiveMode=false`
+
 and Maven will prompt you to enter the rest of the necessary configuration
 parameters (e.g. groupId, artifactId, version and Java package)
--- src/main/resources/archetype-resources/src/test/java/ExampleAccumuloUnitTest.java
+++ src/main/resources/archetype-resources/src/test/java/ExampleAccumuloUnitTest.java
@@ -19,7 +19,19 @@
  */
 package ${package};
 
+import java.util.Map.Entry;
+
+import org.apache.accumulo.core.Constants;
+import org.apache.accumulo.core.client.BatchWriter;
+import org.apache.accumulo.core.client.Connector;
+import org.apache.accumulo.core.client.Instance;
+import org.apache.accumulo.core.client.Scanner;
+import org.apache.accumulo.core.client.ZooKeeperInstance;
+import org.apache.accumulo.core.data.Key;
+import org.apache.accumulo.core.data.Mutation;
+import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.minicluster.MiniAccumuloCluster;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -48,7 +60,54 @@ public class ExampleAccumuloUnitTest {
 
   @Test(timeout = 30000)
   public void test() throws Exception {
-    AccumuloApp.run(accumulo.getInstanceName(), accumulo.getZooKeepers(), "superSecret".getBytes(), new String[0]);
+    // edit this method to play with Accumulo
+
+    Instance instance = new ZooKeeperInstance(accumulo.getInstanceName(), accumulo.getZooKeepers());
+    
+    Connector conn = instance.getConnector("root", "superSecret");
+    
+    conn.tableOperations().create("foo");
+    
+    BatchWriter bw = conn.createBatchWriter("foo", 10000000, 60000, 3);
+    Mutation m = new Mutation("1234");
+    m.put("name", "first", "Alice");
+    m.put("friend", "5678", "");
+    m.put("enemy", "5555", "");
+    m.put("enemy", "9999", "");
+    bw.addMutation(m);
+
+    m = new Mutation("5678");
+    m.put("name", "first", "Bob");
+    m.put("friend", "1234", "");
+    m.put("enemy", "5555", "");
+    m.put("enemy", "9999", "");
+    bw.addMutation(m);
+
+    m = new Mutation("9999");
+    m.put("name", "first", "Eve");
+    m.put("friend", "5555", "");
+    m.put("enemy", "1234", "");
+    m.put("enemy", "5678", "");
+    bw.addMutation(m);
+
+    m = new Mutation("5555");
+    m.put("name", "first", "Mallory");
+    m.put("friend", "9999", "");
+    m.put("enemy", "1234", "");
+    m.put("enemy", "5678", "");
+    bw.addMutation(m);
+
+    bw.close();
+    
+    Scanner scanner = conn.createScanner("foo", Constants.NO_AUTHS);
+    for (Entry<Key,Value> entry : scanner) {
+      System.out.println(entry.getKey() + " " + entry.getValue());
+    }
+   
+    //TODO use scanner to find common enemie ids between Alice and Bob, then
+    //use BatchScanner to look up their names
+ 
+    conn.tableOperations().delete("foo");
   }
   
   @AfterClass
@@ -56,5 +115,4 @@ public class ExampleAccumuloUnitTest {
     accumulo.stop();
     folder.delete();
   }
-  
 }
